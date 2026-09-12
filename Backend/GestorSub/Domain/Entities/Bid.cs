f6bd5f36fe6c6a98733b4bb4927d4332b4073f7b
Estefania Benitez <estefaniaabenitezz@gmail.com>
2026-09-12 20:49:19 -0300

    Creacion de entidades de dominio y relaciones

---
+++ Backend/GestorSub/Domain/Entities/Bid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Bid
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int AuctionId { get; set; }
+        public Auction Auction { get; set; }
+    }
+}
---
+++ Backend/GestorSub/Domain/Entities/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Transaction
+    {
+        public int Id { get; set; }
+        public TransactionType Type { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public int WalletId { get; set; }
+        public Wallet Wallet { get; set; }
+
+    }
+
+    public enum TransactionType
+    {
+        Deposit,
+        Withdrawal,
+        Payment,
+        Refund
+    }
+}
