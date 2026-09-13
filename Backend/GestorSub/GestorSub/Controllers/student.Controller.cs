daf23397c7ce26e379f41bb929363fd679c78656
Nest <zuritanestor57@gmail.com>
2026-09-13 14:59:55 -0300

    provando funciones de git

---
+++ Backend/GestorSub/GestorSub/Controllers/student.Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace GestorSub.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class student : ControllerBase
+    {
+        // probaremos hacer un get 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return  new JsonResult(new { name = "strig"} );
+        }
+    }
+}
